f0b47f66792f6935ce67e788df69deb103240a94
Felipe Facundes <felipe.facundes@gmail.com>
2020-09-10 17:17:02 -0300

    Add files via upload

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 mkdir -p build
-docker build -t ovmf-vbios-patch docker-build
+docker build -t ovmf-vbios-patch ../docker-build
 docker run --rm -v "$PWD/build:/build" -v "$PWD/roms:/roms" ovmf-vbios-patch /ovmf/compile-ovmf.sh gtx-1050ti-maxq.rom
 tar -czf build/ovmf-vbios-patched.tgz build/OVMF*
